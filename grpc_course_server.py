81826493f7b6594b53c5c3836f61fd63dcec668b
Albina <albizuewa@gmail.com>
2025-05-26 22:00:17 -0700

    Add httpx first examples

--- grpc_course_server.py
+++ grpc_course_server.py
@@ -14,12 +14,14 @@ class CourseServiceServicer(course_service_pb2_grpc.CourseServiceServicer):
             description='Будем изучать написание API автотестов'
         )
 
+
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     course_service_pb2_grpc.add_CourseServiceServicer_to_server(servicer=CourseServiceServicer(), server=server)
     server.add_insecure_port('[::]:50051')
     server.start()
     server.wait_for_termination()
+
 
 if __name__ == '__main__':
     serve()
