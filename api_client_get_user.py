249be49de80abc4e5bf5f252f61e22334abff358
Albina <albizuewa@gmail.com>
2025-05-28 20:26:55 -0700

    Add clients usage example

---
+++ api_client_get_user.py
@@ -0,0 +1,25 @@
+from clients.private_http_builder import AuthenticationUserDict
+from clients.users.private_users_client import get_private_users_client
+from clients.users.public_users_client import CreateUserRequestDict, get_public_users_client
+from tools.fakers import get_random_email
+
+
+public_users_client = get_public_users_client()
+create_user_request = CreateUserRequestDict(
+    email=get_random_email(),
+    password='string',
+    lastName='string',
+    firstName='string',
+    middleName='string'
+)
+create_user_response = public_users_client.create_user(request=create_user_request)
+print(f'Greate user data: {create_user_response}')
+
+authentication_user = AuthenticationUserDict(
+    email=create_user_request['email'],
+    password=create_user_request['password']
+)
+private_users_client = get_private_users_client(authentication_user)
+
+get_user_response = private_users_client.get_user(user_id=create_user_response['user']['id'])
+print(f'Get user data: {get_user_response}')
