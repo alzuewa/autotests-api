64f431953194798b2cb2b77c17a08981bd32bdba
Albina <albizuewa@gmail.com>
2025-06-07 22:09:59 -0700

    Add errors schemas and assertions

---
+++ tools/assertions/errors.py
@@ -0,0 +1,20 @@
+from clients.errors_schema import ValidationErrorSchema, ValidationErrorResponseSchema
+from tools.assertions.base import assert_equal, assert_length
+
+
+def assert_validation_error(actual: ValidationErrorSchema, expected: ValidationErrorSchema) -> None | AssertionError:
+    assert_equal(actual.type, expected.type, name='type')
+    assert_equal(actual.input, expected.input, name='input')
+    assert_equal(actual.context, expected.context, name='context')
+    assert_equal(actual.message, expected.message, name='message')
+    assert_equal(actual.location, expected.location, name='location')
+
+def assert_validation_error_response(
+        actual: ValidationErrorResponseSchema,
+        expected: ValidationErrorResponseSchema
+) -> None | AssertionError:
+    assert_length(actual.details, expected.details, name='details')
+
+    for index, detail in enumerate(expected.details):
+        assert_validation_error(actual.details[index], detail)
+
